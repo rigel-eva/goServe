ed0dc39ae995a05e5eab8592741ac97f29d37065
Nora Maguire <eva@rigel.moe>
2018-03-19 10:44:00 -0500

    And we've got a basic web page, neato.

--- src/main.go
+++ src/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"strings"
+	"time"
 )
 
+const port = ":8080"
+
+func serveHello(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Hello " + message
+
+	fmt.Printf("%v : Page Served! %v\n", time.Now(), r.URL.Path)
+	w.Write([]byte(message))
+}
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", serveHello)
+	fmt.Printf("%v : Server started on %v", time.Now(), port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		panic(err)
+	}
 }
