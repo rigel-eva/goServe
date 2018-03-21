b30573bfa39111e580a1e8f58b05255a3f717ee7
Nora Maguire <eva@rigel.moe>
2018-03-20 22:26:45 -0500

    Ok ... another view ... wooooo

--- src/main.go
+++ src/main.go
@@ -14,11 +14,18 @@ func serveHello(ctx iris.Context) {
 	ctx.View("index.html")
 	fmt.Printf("%v : Served Page /", time.Now())
 }
+func serveData(ctx iris.Context) {
+	var name = ctx.Params().Get("name")
+	ctx.ViewData("Name", name)
+	ctx.View("name.html")
+	fmt.Printf("%v : Served Page /%v", time.Now(), name)
+}
 func main() {
 	app := iris.New()
 
 	app.RegisterView(iris.Django("./templates", ".html").Reload(true))
 	app.Get("/", serveHello)
+	app.Get("/hi/{name:string}", serveData)
 	app.Run(addr)
 	fmt.Printf("%v : Server started on %v", time.Now(), addr)
 	fmt.Println("Exited, see you soon!")
